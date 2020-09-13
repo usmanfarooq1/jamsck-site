6b8bbe17e5ce877d78306bdb13a7912899fc6a8e
Syed Usman Farooq <syedusmanfarooq@hotmail.com>
2020-09-13 17:07:33 +0200

    localized data and redirects in case of error

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,23 @@
 const axios= require('axios');
 require('dotenv').config();
+const countries  = require('./countries.json');
 
-module.exports = async function() {
+async function getNews(country){
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
-    return response.data;
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
+    return{
+      "country":country,
+      'articles' : response.data.articles
+    };
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async function() {
+  let newPromises = countries.map(getNews);
+  return Promise.all(newPromises).then(newsObjects=>{
+    console.log('newsObjects : ', newsObjects)
+    return [].concat.apply([],newsObjects);
+  });
 }
